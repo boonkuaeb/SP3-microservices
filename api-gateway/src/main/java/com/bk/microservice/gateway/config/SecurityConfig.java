2b7706259ec5a119f4a9a804d846b3ad174f4615
BKHOMEDESKTOP\boonk <john.kuae@gmail.com>
2024-08-19 23:53:11 +0700

    Spring Boot Microservices Tutorial Part 12 - Observability with Grafana Stack

--- api-gateway/src/main/java/com/bk/microservice/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/bk/microservice/gateway/config/SecurityConfig.java
@@ -14,8 +14,8 @@ import java.util.List;
 @Configuration
 public class SecurityConfig {
 
-    private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/aggregate/**"};
-
+    private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
+            "/swagger-resources/**", "/api-docs/**", "/aggregate/**", "/actuator/prometheus"};
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
--- notification-service/src/main/java/com/bk/microservice/notification/service/NotificationService.java
+++ notification-service/src/main/java/com/bk/microservice/notification/service/NotificationService.java
@@ -1,6 +1,5 @@
 package com.bk.microservice.notification.service;
 
-import com.bk.microservice.order.event.OrderPlacedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,27 +9,27 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class NotificationService {
 
     private final JavaMailSender javaMailSender;
-    @KafkaListener(topics = "order-placed")
-    public void listen(OrderPlacedEvent orderPlacedEvent)
-    {
 
-        log.info("Got message from order-place topic {}",orderPlacedEvent);
+    @KafkaListener(topics = "order-placed")
+    public void listen(com.bk.microservice.order.event.OrderPlacedEvent orderPlacedEvent){
+        log.info("Got Message from order-placed topic {}", orderPlacedEvent);
         MimeMessagePreparator messagePreparator = mimeMessage -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
             messageHelper.setFrom("springshop@email.com");
             messageHelper.setTo(orderPlacedEvent.getEmail().toString());
             messageHelper.setSubject(String.format("Your Order with OrderNumber %s is placed successfully", orderPlacedEvent.getOrderNumber()));
             messageHelper.setText(String.format("""
-                            Hi %s, %s
+                            Hi %s,%s
 
                             Your order with order number %s is now placed successfully.
-
+                            
                             Best Regards
                             Spring Shop
                             """,
@@ -40,11 +39,10 @@ public class NotificationService {
         };
         try {
             javaMailSender.send(messagePreparator);
-            log.info("Order Notification email sent!!");
+            log.info("Order Notifcation email sent!!");
         } catch (MailException e) {
             log.error("Exception occurred when sending mail", e);
             throw new RuntimeException("Exception occurred when sending mail to springshop@email.com", e);
         }
     }
-
 }
